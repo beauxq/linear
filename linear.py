e9d8cbb318ce65d498788fcc4d8f08a0850d987f
beauxq <beauxq@user.noreply.github.com>
2015-10-11 19:08:32 -0500

    not as small as I thought
    suggested scalar on replacement
    
    comment need None in Matrix init
    
    row and column enter (instead of coefficients and equations)
    
    index_of_first_nonzero function
    
    change "end" to "quit" (to prepare for echelon forms)

--- linear.py
+++ linear.py
@@ -10,7 +10,7 @@ class Matrix:
         for i in range(num_of_rows):
             self.row.append([])
             for j in range(num_of_columns):
-                self.row[-1].append(None)  # no values in the matrix
+                self.row[-1].append(None)  # None is a flag for create_matrix function
 
     def __repr__(self):
         to_return = ""
@@ -33,36 +33,40 @@ def create_matrix():
     num_of_rows = 0
     while num_of_rows < 1:
         try:
-            num_of_rows = int(input("number of equations? "))
+            num_of_rows = int(input("number of rows? "))
         except ValueError:
             num_of_rows = 0
 
-    num_of_variables = 0
-    while num_of_variables < 1:
+    num_of_columns = 0
+    while num_of_columns < 1:
         try:
-            num_of_variables = int(input("number of variables? "))
+            num_of_columns = int(input("number of columns? "))
         except ValueError:
-            num_of_variables = 0
+            num_of_columns = 0
 
     # create empty matrix
-    to_return = Matrix(num_of_rows, num_of_variables + 1)  # + 1 for right side of equals
+    to_return = Matrix(num_of_rows, num_of_columns)  # + 1 for right side of equals
 
     # fill matrix with values
     for i in range(num_of_rows):
-        print("equation ", i+1)
-        for j in range(num_of_variables):
+        print("row ", i+1)
+        for j in range(num_of_columns):
             while to_return.row[i][j] is None:
                 try:
-                    to_return.row[i][j] = Fraction(input("coefficient for x" + str(j+1) + "? "))
+                    to_return.row[i][j] = Fraction(input("column " + str(j+1) + "? "))
                 except ValueError:
                     to_return.row[i][j] = None
-        # right side of equals
-        while to_return.row[i][-1] is None:
-            try:
-                to_return.row[i][-1] = Fraction(input("right side of equals? "))
-            except ValueError:
-                to_return.row[i][-1] = None
 
+    return to_return
+
+
+def index_of_first_nonzero(matrix, row_index):
+    """:returns -1 if there are no nonzeros"""
+    to_return = 0
+    while not matrix.row[row_index][to_return]:  # while is zero
+        to_return += 1
+        if to_return == len(matrix.row):  # whole row is zero
+            return -1
     return to_return
 
 
@@ -72,11 +76,15 @@ def main():
     matrix = create_matrix()
     matrix_history.append(deepcopy(matrix))
 
+    # TODO add E echelon form, F rref
+    menu_text = "R Replacement\nI Interchange\nS Scaling\nQ Quit (display operations)\nWhich operation? "
+    valid_input = ["R", "I", "S", "Q"]  # last item should be quit
+
     operation = "N"
-    while operation != "E":  # operations loop
+    while operation != valid_input[-1]:  # operations loop, last item in valid_input should be quit
         print(matrix)
-        while operation not in ["R", "I", "S", "E"]:  # valid input loop
-            operation = input("R Replacement\nI Interchange\nS Scaling\nE End\nWhich operation? ").upper()
+        while operation not in valid_input:  # valid input loop
+            operation = input(menu_text).upper()
         if operation == "R":
             # replacement
             which_row = 0
@@ -91,14 +99,37 @@ def main():
                     other_row = int(input("Which row to add? "))
                 except ValueError:
                     other_row = 0
-            # TODO suggested scalar = (0 - first_nonzero_entry_in_replaced_row) / first_nonzero_entry_in_added_row
+
+            # suggested scalar = (0 - first_nonzero_entry_in_replaced_row) / first_nonzero_entry_in_added_row
             # (if they're in the same column)
+            suggested_scalar = 0
+            first_index = index_of_first_nonzero(matrix, which_row-1)
+            second_index = index_of_first_nonzero(matrix, other_row-1)
+            max_index = max(first_index, second_index)
+            if (second_index > -1) and (first_index > -1):
+                suggested_scalar = Fraction(0 - matrix.row[which_row-1][max_index]) / \
+                                   matrix.row[other_row-1][max_index]
+
             scalar = 0
-            while scalar == 0:  # valid input loop
-                try:
-                    scalar = Fraction(input("With what scalar? "))
-                except ValueError:
-                    scalar = 0
+            if suggested_scalar == 0:
+                while scalar == 0:  # valid input loop
+                    try:
+                        scalar = Fraction(input("With what scalar? "))
+                    except ValueError:
+                        scalar = 0
+            else:  # suggested_scalar != 0
+                input_text = "With what scalar? (Enter for " + str(suggested_scalar.numerator) + \
+                             (("/" + str(suggested_scalar.denominator))
+                              if suggested_scalar.denominator != 1 else "") + ") "
+                while scalar == 0:  # valid input loop
+                    scalar_input = input(input_text)
+                    if scalar_input == "":
+                        scalar = suggested_scalar
+                    else:  # didn't just press enter
+                        try:
+                            scalar = Fraction(scalar_input)
+                        except ValueError:
+                            scalar = 0
 
             for index in range(len(matrix.row[which_row - 1])):
                 matrix.row[which_row - 1][index] += matrix.row[other_row - 1][index] * scalar
@@ -150,7 +181,7 @@ def main():
             matrix_history.append(operation + " (" + str(which_row) + ") * " + str(scalar.numerator) + dependent_str)
 
         # done with operation
-        if operation != "E":
+        if operation != valid_input[-1]:
             matrix_history.append(deepcopy(matrix))
             operation = "N"
 
